90682442683b4962c4e03c18c9d14b8eabf014fd
Partast1 <andersa1989@gmail.com>
2021-04-12 10:02:08 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char string[100];
+printf("Skriv en tekst\n");
+    gets(string);
+    rev(&string);
+    return 0;
+}
+//Tager imod en "string" og vender bogstaverne om
+void rev(char *string){
+   strrev(string);
+      printf( string);
+   return 0;
+}
